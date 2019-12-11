d2fb1bd35cb97212de0b2c2b93a5f160b8e75990
Jason <jas0nle@yahoo.com>
2019-12-11 15:35:56 -0800

    Added base file structure

---
+++ src/components/homepage/HomePageContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class HomePageContainer extends Component {
+
+    render() {
+        return (
+            <div className="about">
+                <p>Before attending Flatiron School's immersive software engineering program, I was working in accounting as my first post-college job. After working for around a year and a half, I decided I wanted to switch careers to a field I had more interest in. Having grown up around computers and in Silicon Valley, my interests have always been in tech, which made pursuing a career in software engineering a natural switch for me. I am excited to apply my skills to technology that improves people’s lives while also continuing to improve and learn from others.</p>
+            </div>
+        )
+    }
+}
+
+export default HomePageContainer;
+
+
+
---
+++ src/components/nav/NavMenu.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+class NavMenu extends Component {
+    render(){
+        return (<div></div>)
+    }
+}
+
+export default NavMenu;
