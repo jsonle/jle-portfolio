b7fe0919b419ccd2c86e72965f3ed0d581b8fb32
Jason <jas0nle@yahoo.com>
2020-01-06 16:13:46 -0600

    Updated about me, header icons, resume

--- src/components/nav/NavMenu.js
+++ src/components/nav/NavMenu.js
@@ -8,24 +8,24 @@ class NavMenu extends Component {
         return (
                 <Menu secondary>
                     <Menu.Item as={Link} to="/" name="about">
-                        About
+                        <strong>About</strong>
                     </Menu.Item>
 
                     <Menu.Item as={Link} to="/projects" name="projects">
-                        Projects
+                        <strong>Projects</strong>
                     </Menu.Item>
 
                     <Menu.Item href={resume} target="_blank" name="resume">
-                        Resume
+                        <strong>Resume</strong>
                     </Menu.Item>
 
                     <Menu.Menu position="right">
                         <Menu.Item href="https://github.com/jsonle" target="_blank" name="github">
-                            <Icon name="github" />
+                            <Icon name="github" size="large" />
                         </Menu.Item>
 
                         <Menu.Item href="https://www.linkedin.com/in/jasondle/" target="_blank" name="linkedin">
-                            <Icon name="linkedin" />
+                            <Icon name="linkedin" size="large" />
                         </Menu.Item>
                     </Menu.Menu>
                 </Menu>
